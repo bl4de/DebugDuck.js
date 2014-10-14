66b97d0580d33e3d76674f524ac43b26abedb061
rafal.janicki <rafal.janicki@accenture.com>
2014-10-14 16:30:46 +0200

    #7 4. Output in console - tests summary; percentage of passed/failed

--- DebugTest.js
+++ DebugTest.js
@@ -74,27 +74,38 @@
                 if (DebugTest.testSuite[__testSuiteName] && DebugTest.testSuite[__testSuiteName].length > 0) {
                     var _total = DebugTest.testSuite[__testSuiteName].length,
                         _passed = 0,
-                        _failed = 0;
+                        _passedPercentage = 0,
+                        _failed = 0,
+                        _failedPercentage = 0;
 
+                    console.log("%c *** DebugTest test suite runner ***   ",
+                        "font-weight:bold; background-color:#ddd; color:#111;");
                     console.log("%c *** " + __testSuiteName + " ***   ",
-                        "font-weight:bold; background-color:#5a5ad4; color:#eee;");
+                        "font-weight:bold; background-color:#ddd; color:#111;");
                     DebugTest.testSuite[__testSuiteName].forEach(function (__test) {
                         if (__test.assertion === true) {
-                            console.log("%c" + " PASSED: " + "%c " + __test.testName + " %c " + __test.message + "  ",
-                                "color:#fff; font-weight:bold; background-color:#206f20;", "font-weight:bold;",
+                            console.log("%c" + " PASSED: " + "%c " + __test.message + "  (" + __test.testName + ")  ",
+                                "color:#fff; font-weight:bold; background-color:#206f20;",
                                 "color:#eee; background-color:#01aa01;");
                             _passed++;
                         } else {
-                            console.warn("%c" + " FAIL:   " + "%c " + __test.testName + " %c Assertion failed  ",
-                                "color:#fff; font-weight:bold; background-color:#a01111;", "font-weight:bold;",
+                            console.warn("%c" + " FAIL:  " + " %c Assertion failed  (" + __test.testName + ")  ",
+                                "color:#fff; font-weight:bold; background-color:#a01111;",
                                 "color:#eee; background-color:#f33;");
                             _failed++;
                         }
                     });
                     console.log("%c ***       SUMMARY       *** ",
-                        "font-weight:bold; background-color:#5a5ad4; color:#eee;");
-                    console.log("%c  passed: " + _passed, "font-weight:normal; color:#206f20;");
-                    console.log("%c  failed: " + _failed, "font-weight:normal; color:#a01111;");
+                        "font-weight:bold; background-color:#ddd; color:#111;");
+
+                    console.log("%c  total tests run: " + _total, "color:#222; font-weight:bold;");
+                    _passedPercentage = parseInt((_passed / _total) * 100, 10);
+                    _failedPercentage = parseInt((_failed / _total) * 100, 10);
+
+                    console.log("%c  passed: " + _passed + " (" + _passedPercentage + "%)  ",
+                        "font-weight:normal; color:#206f20;");
+                    console.log("%c  failed: " + _failed + " (" + _failedPercentage + "%)  ",
+                        "font-weight:normal; color:#a01111;");
                 }
             };
 
